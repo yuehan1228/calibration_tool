5a1d34b30d3a3365cfa5a666062659d14d02264f
穿方裤的海绵宝宝 <yuehan20001228@163.com>
2026-05-08 12:05:05 +0800

    deploy: 适配生产环境，添加 gunicorn + Procfile

--- app.py
+++ app.py
@@ -102,4 +102,6 @@ def calculate():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', '0') == '1'
+    app.run(debug=debug, host='0.0.0.0', port=port)
